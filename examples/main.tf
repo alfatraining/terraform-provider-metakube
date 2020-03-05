0d5e8c46c0e79cd5fc82231347e21616559334be
furkhat <vailodf@gmail.com>
2020-03-05 16:24:49 +0600

    remove old comment

--- examples/main.tf
+++ examples/main.tf
@@ -11,17 +11,6 @@ resource "metakube_project" "my-project" {
 }
 
 resource "metakube_cluster" "my-cluster" {
-  # If you use API Account's token to create a project and cluster for it
-  # it WILL FAIL
-  # This is due to ownership issues with API Accounts and user account that 
-  # it belongs to. So referencing as below wont work:
-  # project_id = metakube_project.my-project.id
-  # 
-  # To create a cluster you should either use token that belongs to user account
-  # itself and not to api account or create a project on UI and set explicit 
-  # values for project_id:
-  # project_id = "explicit-project-id"
-
   project_id = metakube_project.my-project.id
 
   name    = "my-cluster"
