e1cfb976ef6652e2f65b59f6567f1a06a5dba9b0
furkhat <vailodf@gmail.com>
2020-03-05 10:50:54 +0600

    describe ownership issue with project id

--- examples/main.tf
+++ examples/main.tf
@@ -11,7 +11,15 @@ resource "metakube_project" "my-project" {
 }
 
 resource "metakube_cluster" "my-cluster" {
-  project_id = metakube_project.my-project.id
+  # If you use API Account's token to create a project and cluster for it
+  # it WILL FAIL
+  # This is due to ownership issues with API Accounts and user account that 
+  # it belongs to. So referencing as below wont work:
+  # project_id = metakube_project.my-project.id
+  # 
+  # To create a cluster you should either use other kind of token that doesn't 
+  # have ownership issue or create a project on UI and set here explicitly.
+  project_id = "5hrnkmpmp4"
 
   name    = "my-cluster"
   version = "1.17.3"
